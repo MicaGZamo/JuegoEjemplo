b12e6a84f029188bccfe443e7c41ebe2afcf991f
MicaelaG.Zamo <micazamo1@gmail.com>
2024-03-19 11:31:03 -0300

    Update JuegoEjemplo
    Mensaje al iniciar el juego

--- src/juegoejemplo/JuegoEjemplo.java
+++ src/juegoejemplo/JuegoEjemplo.java
@@ -7,12 +7,18 @@ public class JuegoEjemplo {
    
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
         Auto ford = new Auto("rojo", 10,5);
         Escena escena1=new Escena(ford);
+        
+                      
+        System.out.println("--------- COMIENZA EL JUEGO ---------");
         escena1.jugar();
       
       
-        
+       
         
         
     }
