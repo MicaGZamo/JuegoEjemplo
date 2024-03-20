68e5ae37c96aa68a70b73ac742225b948583f94c
Gaspar Nicolas Viola <gaspar5454@hotmail.com>
2024-03-20 10:56:59 -0300

    Agregando Auto y sout

--- src/juegoejemplo/JuegoEjemplo.java
+++ src/juegoejemplo/JuegoEjemplo.java
@@ -7,19 +7,19 @@ public class JuegoEjemplo {
    
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
-        
-        
+                       
         Auto ford = new Auto("rojo", 10,5);
         Escena escena1=new Escena(ford);
         
         Auto chevrolet = new Auto("verde",20,3);
-        Escena escena2 = new Escena(chevrolet);
+        Escena escena2 = new Escena(chevrolet); //Agregue un auto y una Escena 
         
                       
         System.out.println("--------- COMIENZA EL JUEGO ---------");
         escena1.jugar();
+        
+        System.out.println("--------- PROBAMOS CON OTRO AUTO ---------");
+        escena2.jugar();
       
         System.out.println("bye");
        
